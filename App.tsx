fa48ab1253b1696bca720a6d96b2f78f114294c1
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-02-26 17:56:27 -0300

    feat: add react navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import { CategoriesScreen } from "./src/screens/Categories";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
