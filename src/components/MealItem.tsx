022bdbc1297289bc4f479e6740e6681bd8fc6aa9
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-02-27 08:44:52 -0300

    feat: add props and styles to mealItem

--- src/components/MealItem.tsx
+++ src/components/MealItem.tsx
@@ -1,12 +1,83 @@
-import { Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+} from "react-native";
 import Meal from "../models/meal";
 
-type MealItem = Pick<Meal, 'title'>
+export type MealItemProps = Pick<
+  Meal,
+  "title" | "imageUrl" | "duration" | "affordability" | "complexity"
+>;
 
-export function MealItem({ title }: MealItem) {
+export function MealItem({
+  title,
+  imageUrl,
+  duration,
+  affordability,
+  complexity,
+}: MealItemProps) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+
+        <View style={styles.detailsContainer}>
+          <Text style={styles.detailItem}>{duration} min</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "black",
+    shadowRadius: 8,
+    shadowOpacity: 0.35,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center",
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
--- src/screens/MealsOverview.tsx
+++ src/screens/MealsOverview.tsx
@@ -2,36 +2,54 @@ import { StyleSheet, Text, View, FlatList } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import { useRoute } from "@react-navigation/native";
 
-import type { RootStackParamList } from "../../App";
-import type { RouteProp } from "@react-navigation/native";
 import { MealItem } from "../components/MealItem";
 import Meal from "../models/meal";
 
-type renderMealItemProps = Pick<Meal, 'title'>
+import type { MealItemProps } from "../components/MealItem";
+import type { RootStackParamList } from "../../App";
+import type { RouteProp } from "@react-navigation/native";
 
 export function MealsOverviewScreen() {
-  const route = useRoute<RouteProp<RootStackParamList, 'MealsOverview'>>();
+  const route = useRoute<RouteProp<RootStackParamList, "MealsOverview">>();
 
   const { categoryId } = route.params;
 
-  const displayedMeals = MEALS.filter(meal => {
+  const displayedMeals = MEALS.filter((meal) => {
     return meal.categoryIds.indexOf(categoryId) >= 0;
   });
 
-  function renderMealItem({ title }: renderMealItemProps) {
-    return <MealItem title={title} />
+  function renderMealItem({
+    title,
+    imageUrl,
+    affordability,
+    complexity,
+    duration,
+  }: MealItemProps) {
+    return (
+      <MealItem
+        title={title}
+        imageUrl={imageUrl}
+        affordability={affordability}
+        complexity={complexity}
+        duration={duration}
+      />
+    );
   }
 
   return (
     <View style={styles.container}>
-      <FlatList data={displayedMeals} keyExtractor={item => item.id} renderItem={({ item }) => renderMealItem(item)} />
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => renderMealItem(item)}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-  }
-})
+  },
+});
