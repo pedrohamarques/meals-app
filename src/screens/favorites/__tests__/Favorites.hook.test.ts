c0ccdc3baa566b11ed7d387a2c368c2d00840faa
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-03-06 19:42:40 -0300

    fix: fix favorite hook screen tests

--- src/screens/favorites/__tests__/Favorites.hook.test.ts
+++ src/screens/favorites/__tests__/Favorites.hook.test.ts
@@ -2,16 +2,19 @@ import { renderHook } from "@testing-library/react-native";
 
 import { useFavoritesScreen } from "../Favorites.hook";
 
+import { DUMMY_DATA } from "./fixture";
+
 const mockAppState = {
   favorites: {
-    ids: ["m1", "m2"],
+    ids: ["m1"],
   },
 };
 
 const mockUseSelector = jest.fn();
 
-jest.mock("../../../hooks/redux", () => ({
-  useAppSelector: () => mockUseSelector(),
+jest.mock("react-redux", () => ({
+  //@ts-ignore
+  useSelector: (callback) => mockUseSelector(callback),
 }));
 
 describe("screens/favorites/useFavoritesScreen", () => {
@@ -21,11 +24,10 @@ describe("screens/favorites/useFavoritesScreen", () => {
 
   it("returns favorite meals when ids are provided", () => {
     mockUseSelector.mockImplementation((callback) => callback(mockAppState));
+    const { result } = renderHook(() => useFavoritesScreen());
+
+    expect(mockUseSelector).toHaveBeenCalledTimes(1);
+
+    expect(result.current.favoriteMeals).toEqual(DUMMY_DATA);
   });
-
-  const { result } = renderHook(() => useFavoritesScreen());
-
-  expect(mockUseSelector).toHaveBeenCalledTimes(1);
-
-  expect(result.current.favoriteMeals).toEqual([]);
 });
